109eea17746e4f35ec65302fc03609a54d218ac2
Lvov Nikita <LvovNikita>
2022-11-17 14:31:57 +0300

    Add some calendar logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,28 @@
 import { createStore } from 'vuex';
 
 export default createStore({
+    // TODO: add models
     state: {
         // FIXME: just for testing purposes
         templates: [
-            { id: 1, name: 'Workday routine' },
-            { id: 2, name: 'Weekend routine' }
+            { id: 1, name: 'Workday routine', blockIds: [] },
+            { id: 2, name: 'Weekend routine', blockIds: [] }
         ],
         blocks: [
+            // FIXME: is tempalteId necessary?
             { name: 'Block1', startTime: '14:00', endTime: '15:00', templateId: 1 },
             { name: 'Block2', startTime: '13:00', endTime: '16:00', templateId: 2 }
+        ],
+        routines: [
+            { date: '2022-11-20', templateId: 1 }
         ]
     },
     getters: {
         template: state => (id: string | number) => {
             return state.templates.find(template => template.id == id)
+        },
+        templates: state => () => {
+            return state.templates
         },
         blocks: state => (templateId: string | number) => {
             return state.blocks.filter(block => block.templateId == templateId)
@@ -24,8 +32,11 @@ export default createStore({
         addTemplate(state, template) {
             state.templates = [...state.templates, template]
         },
-        addBlock(state, block: any) {
+        addBlock(state, block) {
             state.blocks.push(block)
+        },
+        addRoutine(state, routine) {
+            state.routines.push(routine)
         }
     },
     actions: {},
--- src/views/calendar/CalendarPage.vue
+++ src/views/calendar/CalendarPage.vue
@@ -1,46 +1,134 @@
 <template>
     <ion-page>
+        
         <ion-header>
             <ion-toolbar>
                 <ion-title>Calendar</ion-title>
             </ion-toolbar>
         </ion-header>
-        <ion-content :fullscreen="true">
-            <ion-header collapse="condense">
-                <ion-toolbar>
-                    <ion-title size="large">Tab 3</ion-title>
-                </ion-toolbar>
-            </ion-header>
 
+        <ion-content class="ion-padding">
 
+            <ion-datetime
+                ref="datetime"
+                presentation="date" 
+                :first-day-of-week="1" 
+                :multiple="true"
+                v-model="pickedDates"
+                >
+            </ion-datetime>
 
-            <ExploreContainer name="Tab 3 page" />
+            <ion-list>
+                <ion-item>
+                    <ion-select placeholder="Select template" v-model="selectedTemplateId">
+                        <ion-select-option 
+                            v-for="template of templates" 
+                            :key="template.id" 
+                            :value="template.id">
+                            {{ template.name }}
+                        </ion-select-option>
+                    </ion-select>
+                </ion-item>
+            </ion-list>
+            
+            <ion-button @click="addToCalendar(); reset()" :disabled="isButtonDisabled">Apply</ion-button>
+            
+            <!-- FIXME: for testing purposes only -->
+            {{ selectedTemplateId }}: {{ pickedDates }}
+            {{ areDatesPicked }} {{ isTemplateSelected }}
+
         </ion-content>
+
     </ion-page>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref } from 'vue';
 
     import { 
         IonPage, 
         IonHeader, 
         IonToolbar, 
         IonTitle, 
-        IonContent 
+        IonContent,
+        IonDatetime,
+        IonButton,
+        IonList,
+        IonItem,
+        IonSelect,
+        IonSelectOption
     } from '@ionic/vue';
     
-    import ExploreContainer from '@/components/ExploreContainer.vue';
 
     export default defineComponent({
         name: 'Tab3Page',
         components: { 
-            ExploreContainer, 
             IonHeader, 
             IonToolbar, 
             IonTitle, 
             IonContent, 
-            IonPage 
+            IonPage,
+            IonDatetime,
+            IonButton,
+            IonList,
+            IonItem,
+            IonSelect,
+            IonSelectOption
+        },
+        data: () => {
+            return {
+                pickedDates: '',
+                selectedTemplateId: ''
+            }
+        },
+        computed: {
+            templates() {
+                return this.$store.getters.templates()
+            },
+            areDatesPicked() {
+                if (!Array.isArray(this.pickedDates)) return false
+                return this.pickedDates.length > 0 ? true : false
+            },
+            isTemplateSelected() {
+                return !!this.selectedTemplateId
+            },
+            isButtonDisabled() {
+                return !(this.areDatesPicked && this.isTemplateSelected)
+            }
+        },
+        methods: {
+            addToCalendar() {
+                if (this.areDatesPicked && this.isTemplateSelected) {
+                    for (const date of this.pickedDates) {
+                        this.$store.commit('addRoutine', {
+                            date,
+                            templateId: this.selectedTemplateId
+                        })
+                    }
+                    // const activeDaysElems = document.querySelectorAll('.calendar-day-active')
+                    // for (const elem of activeDaysElems) {
+                    //     elem.classList.remove("calendar-day-active")
+                    // }
+                    this.pickedDates = ''
+                    this.selectedTemplateId = ''
+                }
+            }
+        },
+        setup() {
+            const datetime = ref()
+            // FIXME: example from docs that doesn't work
+            const reset = () => {   
+                // console.log('RESET!')            
+                // return datetime.value.$el.reset() 
+            }
+            return { datetime, reset }
         }
     });
 </script>
+
+<style>
+    ion-datetime {
+        margin: 1em auto 1em auto;
+        max-width: 550px
+    }
+</style>
